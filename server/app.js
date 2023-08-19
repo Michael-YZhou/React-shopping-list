e031963e77d10d3e2917203f50ce2d4b56d20e7c
Yang Zhou <yangzhouinter@gmail.com>
2023-08-19 10:14:21 +1000

    update url and cors setting

--- server/app.js
+++ server/app.js
@@ -1,3 +1,5 @@
+import cors from "cors";
+
 const express = require("express");
 require("dotenv").config();
 
@@ -9,6 +11,13 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
+
+app.use(
+  cors({
+    origin: "https://react-shopping-list.onrender.com", //replace with your render API URL
+  })
+);
+app.options("*", cors());
 
 app.use(logger);
 app.use("/api/item", itemController);
